3b43b2a6b3d013c418843be6581960f7151d0af9
level2isbetter <white_llama@live.com>
2016-12-09 15:58:12 -0500

    Add files via upload

---
+++ dank.py
@@ -0,0 +1,33 @@
+import discord
+import asyncio
+import importlib
+import json
+import sys
+import on_msg
+
+# Load bot info
+bot_info = none
+with open('bot_info.json') as f:
+    bot_info = json.load(f)
+
+client = discord.Client()
+
+@client.async_event
+def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    yield from client.change_presence(game=discord.Game(name='boi'))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('/hi'):
+        await client.send_message(message.channel, 'hi!!!! xD')
+
+@client.event
+async def on_reaction_add(reaction, user):
+    await client.send_message(reaction.message.channel, "The reaction added was: " + str(reaction.emoji))
+    await client.delete_message(reaction.message)
+
+client.run('MjU2NjI5NTQwOTYzNjE0NzIw.CywisA.cttD_e4F3mZP2uYxJWZ1ZSSfTHI')
